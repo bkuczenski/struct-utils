c147563c2344989d7ba2de2a6d16c4290dc619ca
Brandon Kuczenski <bkuczenski@bren.ucsb.edu>
2014-05-28 16:04:11 -0700

    approx.m apply near-zero threshold.

--- util/approx.m
+++ util/approx.m
@@ -1,7 +1,17 @@
 function C=approx(A,B,tol)
 % function C=approx(A,B,tol)
 % Like eq, but allows for small variation.  approx(A,B,tol) returns true if
-% abs(A-B)./A<tol.  Default tol is 1e-5.
+% abs(A-B)./A<tol.  
+%
+% if B==0, returns true if abs(A)<tol.  Likewise for A==0.
+%
+% Default tol is 1e-5.
 if nargin<3 ; tol=1e-5; end
 
-C=(abs(A-B)./A)<tol;
+Ia=find(A==0);
+Ib=find(B==0);
+Ic=(~Ia & ~Ib);
+
+C(Ia)=(abs(B(Ia))<tol);
+C(Ib)=(abs(A(Ib))<tol);
+C(Ic)=(abs(A(Ic)-B(Ic))./A(Ic))<tol;
