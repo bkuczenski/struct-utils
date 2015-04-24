699d97c242d57b0c8b27f5ced70372fd9d248800
Brandon Kuczenski <bkuczenski@bren.ucsb.edu>
2015-04-24 16:28:28 -0700

    help text for cisnan.m

--- util/cisnan.m
+++ util/cisnan.m
@@ -1,4 +1,6 @@
 function C=cisnan(C)
+% function b=cisnan(C)
+% returns a logical array the same size as C with 1 where C is NaN and 0 otherwise.
 % 
 C(~cellfun(@isnumeric,C))=deal({0});
 C=cellfun(@isnan,C);
